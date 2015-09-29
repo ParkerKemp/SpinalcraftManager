e32dd1a9393387d815f12f15dc7b2447e9584dca
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-09-29 14:02:02 -0500

    Add an intent filter for spinal:// URIs

--- app/src/main/java/com/example/parker/spinalcraftmanager/Connector.java
+++ app/src/main/java/com/example/parker/spinalcraftmanager/Connector.java
@@ -1,6 +1,8 @@
 package com.example.parker.spinalcraftmanager;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -13,7 +15,10 @@ public class Connector implements Runnable {
             socket = new Socket();
             socket.connect(new InetSocketAddress("192.168.0.14", 9494), 5000);
             if(socket.isConnected()){
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 System.out.println("Connected!");
+                String response = reader.readLine();
+                System.out.println(response);
             }
             else{
                 System.out.println("Not Connected!");
