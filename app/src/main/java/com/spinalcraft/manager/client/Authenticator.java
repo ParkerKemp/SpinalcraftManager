efbefab274cc0404fb8620892331e21822eef565
ParkerKemp <charles.kemp702@topper.wku.edu>
2015-10-02 21:47:57 -0500

    Generate keys once and store in local files

--- app/src/main/java/com/spinalcraft/manager/client/Authenticator.java
+++ app/src/main/java/com/spinalcraft/manager/client/Authenticator.java
@@ -1,41 +1,84 @@
 package com.spinalcraft.manager.client;
 
-import android.text.style.AlignmentSpan;
-import android.util.Base64;
+import android.app.Activity;
+import android.content.Context;
 
-import com.spinalcraft.easycrypt.EasyCrypt;
-
-import java.nio.charset.StandardCharsets;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.security.KeyFactory;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
 
 /**
  * Created by Parker on 10/2/2015.
  */
 public class Authenticator {
-    public Authenticator(){
+    private Crypt crypt;
+    private MainActivity activity;
 
-        Crypt crypt = new Crypt();
+    public Authenticator(MainActivity activity){
+        crypt = new Crypt();
+        verifyKeysExist();
+    }
+
+    public String getPublicKey(){
+        return getKey(".publickey");
+    }
+
+    public String getPrivateKey(){
+        return getKey(".privatekey");
+    }
+
+    private String getKey(String filename){
+        byte[] buffer = new byte[100];
+        try {
+            FileInputStream inputStream = activity.openFileInput(filename);
+            inputStream.read(buffer, 0, 100);
+            return new String(buffer);
+        } catch (FileNotFoundException e) {
+            generateKeys();
+            return getKey(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void verifyKeysExist(){
+        try {
+            activity.openFileInput(".publickey");
+            activity.openFileInput(".privatekey");
+        } catch (FileNotFoundException e) {
+            generateKeys();
+            System.out.println("Generating keys for the first time");
+        }
+    }
+
+    private void generateKeys(){
         try {
             KeyPair keyPair = crypt.generateKeys();
-
             PrivateKey privateKey = keyPair.getPrivate();
-            String privateString = crypt.stringFromPrivateKey(privateKey);
+            PublicKey publicKey = keyPair.getPublic();
 
-            System.out.println("Private key from EasyCrypt: " + privateString);
+            byte[] bytes = crypt.stringFromPrivateKey(privateKey).getBytes();
+            FileOutputStream outputStream = activity.openFileOutput(".privatekey", Context.MODE_PRIVATE);
+            outputStream.write(bytes, 0, bytes.length);
+
+            bytes = crypt.stringFromPublicKey(publicKey).getBytes();
+            outputStream = activity.openFileOutput(".publickey", Context.MODE_PRIVATE);
+            outputStream.write(bytes, 0, bytes.length);
 
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
